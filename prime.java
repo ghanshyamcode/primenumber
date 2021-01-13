f76eaf4e0cc6fe5a5fdb2dde61837e2d0ea4c220
Ghanshyam verma <76841340+ghanshyamcode@users.noreply.github.com>
2021-01-13 20:23:40 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,27 @@
+public class prime {
+    public static void main (String args[]){
+        int i,m,n=0, flag=0;
+        n=n+Integer.parseInt(args[0]);
+        m=n/2;
+        if (n==0|| n==1){
+            System.out.println(n+" is not prime number");
+        }
+        else{
+            for (i=2; i<=m;i++){
+                if (n%i==0){
+                    System.out.println(n+" is not a prime number");
+                    flag=1;
+                    break;
+                }
+            }
+        if (flag==0){
+            System.out.println(n+" is a prime number");
+        }    
+        }
+
+
+        
+        
+
+    }
+}
